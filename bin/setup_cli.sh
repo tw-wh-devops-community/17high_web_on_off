d1c5ef17a2e22c56610f96d2d12952dbc5bf0a84
Xunjie Zhou <xunjie.zhou@vodafone.com.au>
2017-04-14 23:05:06 +0800

    fix dir

--- bin/setup_cli.sh
+++ bin/setup_cli.sh
@@ -1,7 +1,7 @@
 #! /bin/bash
-mkdir ~/.aws
-touch ~/.aws/config
-touch ~/.aws/credentials
+mkdir /home/travis/.aws
+touch /home/travis/.aws/config
+touch /home/travis/.aws/credentials
 echo "region = cn-norht-1" >> ~/.aws/config
 echo "aws_access_key_id = $AWS_ACCESS_KEY_ID" >> ~/.aws/credentials
 echo "aws_secret_access_key = = $AWS_ACCESS_KEY" >> ~/.aws/credentials
