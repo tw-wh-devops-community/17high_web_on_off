c30598d8e1dbdcd1319a02719442cdc5fc3794f2
Xunjie Zhou <xunjie.zhou@vodafone.com.au>
2017-04-14 23:27:16 +0800

    fix aws credentials file

--- bin/setup_cli.sh
+++ bin/setup_cli.sh
@@ -1,9 +1,10 @@
 #! /bin/bash
-mkdir /home/travis/.aws
-touch /home/travis/.aws/config
-touch /home/travis/.aws/credentials
+mkdir ~/.aws
+touch ~/.aws/config
+touch ~/.aws/credentials
 echo "[default]" >> ~/.aws/config
 echo "region = cn-north-1" >> ~/.aws/config
+echo "[default]" >> ~/.aws/credentials
 echo "aws_access_key_id = $AWS_ACCESS_KEY_ID" >> ~/.aws/credentials
 echo "aws_secret_access_key = $AWS_ACCESS_KEY" >> ~/.aws/credentials
 cat /home/travis/.aws/config
