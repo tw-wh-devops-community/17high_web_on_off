4ba0be632b8d20ce88cdc6d5ea9f7c991d876e9d
Xunjie Zhou <xunjie.zhou@vodafone.com.au>
2017-04-15 00:27:46 +0800

    fix travis ci

--- bin/turn_aws_ec2_on_off.sh
+++ bin/turn_aws_ec2_on_off.sh
@@ -1,4 +1,10 @@
 #! /bin/bash
 instanceId=$(eval "aws ec2 describe-instances --filters 'Name=tag:Name,Values="$EC2_INSTANCE_NAME"' --output text --query 'Reservations[*].Instances[*].InstanceId'")
-echo "Stop instance $instanceId"
-aws ec2 stop-instances --instance-ids $instanceId
+state=$(eval "aws ec2 describe-instances --filters 'Name=tag:Name,Values="$EC2_INSTANCE_NAME"' --output text --query 'Reservations[*].Instances[*].State.Name'")
+if [ "$state" == "stopped" ]; then
+    echo "Start instance $instanceId"
+    aws ec2 start-instances --instance-ids $instanceId
+else
+    echo "Stop instance $instanceId"
+    aws ec2 stop-instances --instance-ids $instanceId
+fi
