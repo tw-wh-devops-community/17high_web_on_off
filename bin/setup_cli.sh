2350906a51bc0379742a3fa28f501a1c2463950f
Xunjie Zhou <xunjie.zhou@vodafone.com.au>
2017-04-14 23:13:24 +0800

    fix region name

--- bin/setup_cli.sh
+++ bin/setup_cli.sh
@@ -2,7 +2,7 @@
 mkdir /home/travis/.aws
 touch /home/travis/.aws/config
 touch /home/travis/.aws/credentials
-echo "region = cn-norht-1" >> ~/.aws/config
+echo "region = cn-north-1" >> ~/.aws/config
 echo "aws_access_key_id = $AWS_ACCESS_KEY_ID" >> ~/.aws/credentials
 echo "aws_secret_access_key = = $AWS_ACCESS_KEY" >> ~/.aws/credentials
 cat /home/travis/.aws/config
