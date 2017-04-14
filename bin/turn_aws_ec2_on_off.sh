30bea9f25fb4487c99eaef8cfb74e65b418a8f77
Xunjie Zhou <xunjie.zhou@vodafone.com.au>
2017-04-15 00:11:04 +0800

    fix script to get instance id

--- bin/turn_aws_ec2_on_off.sh
+++ bin/turn_aws_ec2_on_off.sh
@@ -1,4 +1,4 @@
 #! /bin/bash
-instanceId=$(aws ec2 describe-instances --filters 'Name=tag:Name,Values=$EC2_INSTANCE_NAME' --output text --query 'Reservations[*].Instances[*].InstanceId')
+instanceId=$(eval "aws ec2 describe-instances --filters 'Name=tag:Name,Values="$EC2_INSTANCE_NAME"' --output text --query 'Reservations[*].Instances[*].InstanceId'")
 echo "Stop instance $instanceId"
 aws ec2 stop-instances --instance-ids $instanceId
