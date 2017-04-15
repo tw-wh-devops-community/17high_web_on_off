4a891dd245dceb50cc663a6ae5bbcf97faf28f69
Xunjie Zhou <xunjie.zhou@vodafone.com.au>
2017-04-15 22:46:12 +0800

    Stop the server only, when it's not in stopped state

--- bin/turn_aws_ec2_on_off.sh
+++ bin/turn_aws_ec2_on_off.sh
@@ -2,8 +2,7 @@
 instanceId=$(eval "aws ec2 describe-instances --filters 'Name=tag:Name,Values="$EC2_INSTANCE_NAME"' --output text --query 'Reservations[*].Instances[*].InstanceId'")
 state=$(eval "aws ec2 describe-instances --filters 'Name=tag:Name,Values="$EC2_INSTANCE_NAME"' --output text --query 'Reservations[*].Instances[*].State.Name'")
 if [ "$state" == "stopped" ]; then
-    echo "Start instance $instanceId"
-    aws ec2 start-instances --instance-ids $instanceId
+    echo "Instance $instanceId has already been stopped."
 else
     echo "Stop instance $instanceId"
     aws ec2 stop-instances --instance-ids $instanceId
