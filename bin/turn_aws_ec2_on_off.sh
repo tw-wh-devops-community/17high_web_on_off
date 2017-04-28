1f538e23f76f5f6128795838a1b31bc6b6983657
Lu Chen <blacksea.chen@hotmail.com>
2017-04-28 10:16:26 +0800

    update script to start instance

--- bin/turn_aws_ec2_on_off.sh
+++ bin/turn_aws_ec2_on_off.sh
@@ -1,9 +1,9 @@
 #! /bin/bash
 instanceId=$(eval "aws ec2 describe-instances --filters 'Name=tag:Name,Values="$EC2_INSTANCE_NAME"' --output text --query 'Reservations[*].Instances[*].InstanceId'")
 state=$(eval "aws ec2 describe-instances --filters 'Name=tag:Name,Values="$EC2_INSTANCE_NAME"' --output text --query 'Reservations[*].Instances[*].State.Name'")
-if [ "$state" == "started" ]; then
-    echo "Instance $instanceId has already been started."
+if [ "$state" == "running" ]; then
+    echo "Instance $instanceId is running."
 else
-    echo "Sart instance $instanceId"
+    echo "Start instance $instanceId"
     aws ec2 start-instances --instance-ids $instanceId
 fi
